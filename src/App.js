b289ab977659e00122c6eb477cec61735459f5dc
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-01-29 21:48:33 +0200

    added AuthNavigator + Screens

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import RootNavigator from "./navigations/RootNavigation";
-import { createStackNavigator } from "@react-navigation/stack";
 import AuthNavigation from "./navigations/AuthNavigation";
 
 export default function App() {
-  const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
       <StatusBar style="auto"></StatusBar>
-      <RootNavigator></RootNavigator>
+
+      {false ? <RootNavigator></RootNavigator> : <AuthNavigation></AuthNavigation>}
+
     </NavigationContainer>
   );
 }
---
+++ src/navigations/AuthNavigation.jsx
@@ -0,0 +1,14 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../screens/auth/Login";
+import Register from "../screens/auth/Register";
+
+export default function AuthNavigator() {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}></Stack.Screen>
+      <Stack.Screen name="Register" component={Register}></Stack.Screen>
+    </Stack.Navigator>
+  );
+}
