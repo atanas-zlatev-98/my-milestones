3128e90cc2ddef2cf47dcbf6e85f9ca9c7375c6c
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-02-26 00:41:16 +0200

    added register validation name and email max length

--- src/validation/validationSchemas.js
+++ src/validation/validationSchemas.js
@@ -2,8 +2,8 @@ import {z} from 'zod';
 
 export const registerSchema = z.object({
   imageUri:z.string().nullable().refine(val => val !== null, {message: "Please select a profile picture"}),
-  name: z.string().min(1, "Name is required").min(6, "Name must be at least 6 characters"),
-  email: z.string().min(1, "Email is required").email("Invalid email address"),
+  name: z.string().min(1, "Name is required").min(6, "Name must be at least 6 characters").max(25,'Name must not exceed 25 characters'),
+  email: z.string().min(1, "Email is required").email("Invalid email address").max(50,'Email must not exceed 50 characters'),
   password: z.string().min(1, "Password is required").min(6, "Password must be at least 6 characters").regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/,
       { message: "Must contain at least one letter and one number" }),
   confirmPassword: z.string().min(1, "Please confirm your password")
